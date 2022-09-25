6ab15c359696025e1c7f24be569022c2fcec309a
456477617264 <ec557782@gmail.com>
2022-09-25 23:45:15 +0100

    change description

--- .eleventy.js
+++ .eleventy.js
@@ -3,23 +3,24 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 // Base setup for builds, needed for og tags and correct image paths
 // (mostly for github pages deployment, see build-deploy.yaml)
-const baseUrl = process.env.BASE_URL || 'http://localhost:8080';
+const baseUrl = process.env.BASE_URL || "http://localhost:8080";
 // e.g. 'https://mandrasch.github.io/'
-const pathPrefix = process.env.PATH_PREFIX || '/';
+const pathPrefix = process.env.PATH_PREFIX || "/";
 // e.g. '/11ty-plain-boostrap5/'
-console.log('baseUrl is set to ...', baseUrl);
-console.log('pathPrefix is set to ...', pathPrefix);
+console.log("baseUrl is set to ...", baseUrl);
+console.log("pathPrefix is set to ...", pathPrefix);
 
 // will be accessible in all templates via
 // see "eleventyConfig.addGlobalData("site", globalData);"" below
 // related: https://github.com/11ty/eleventy/issues/1641
 const globalSiteData = {
   title: "The Codhead Club",
-  description: "Template for static site generator Eleventy with Boostrap 5 and SCSS/JS compilation via laravel-mix.",
-  locale: 'en',
+  description:
+    "The Codhead Club is an initiative run by technical professionals to aid social, voluntary and charity sector organisations access advice and skills which they may not otherwise have been able to afford. In addition, we contribute to projects and events for social good.",
+  locale: "en",
   baseUrl: baseUrl,
   pathPrefix: pathPrefix,
-}
+};
 
 // https://www.11ty.dev/docs/plugins/image/#use-this-in-your-templates
 const Image = require("@11ty/eleventy-img");
@@ -33,20 +34,23 @@ async function imageShortcode(src, alt, sizes = "100vw") {
   // TODO: pathPrefix must be '/path/', check existence of trailing slash?!
   let metadata = await Image(src, {
     widths: [600, 1200],
-    formats: ['webp', 'jpeg'],
+    formats: ["webp", "jpeg"],
     urlPath: `${pathPrefix}img`,
     // outputDir: "./img/" is default
-    outputDir: './_site/img/' // passthrough below didn't work, write to output dir by now
-
+    outputDir: "./_site/img/", // passthrough below didn't work, write to output dir by now
   });
 
   let lowsrc = metadata.jpeg[0];
   let highsrc = metadata.jpeg[metadata.jpeg.length - 1];
 
   return `<picture>
-    ${Object.values(metadata).map(imageFormat => {
-    return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`;
-  }).join("\n")}
+    ${Object.values(metadata)
+      .map((imageFormat) => {
+        return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat
+          .map((entry) => entry.srcset)
+          .join(", ")}" sizes="${sizes}">`;
+      })
+      .join("\n")}
       <img
         src="${lowsrc.url}"
         width="${highsrc.width}"
@@ -58,7 +62,6 @@ async function imageShortcode(src, alt, sizes = "100vw") {
 }
 
 module.exports = function (eleventyConfig) {
-
   // Set site title
   eleventyConfig.addGlobalData("site", globalSiteData);
 
@@ -83,7 +86,7 @@ module.exports = function (eleventyConfig) {
 
   // Watch for changes (and reload browser)
   eleventyConfig.addWatchTarget("./src/assets"); // normal (static) assets
-  eleventyConfig.addWatchTarget("./dist") // laravel-mix output changes
+  eleventyConfig.addWatchTarget("./dist"); // laravel-mix output changes
 
   // RandomId function for IDs used by labelled-by
   // Thanks https://github.com/mozilla/nunjucks/issues/724#issuecomment-207581540
@@ -108,6 +111,6 @@ module.exports = function (eleventyConfig) {
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk",
     // important for github pages build (subdirectory):
-    pathPrefix: pathPrefix
+    pathPrefix: pathPrefix,
   };
 };
