f05dd56040b10f5b372422bf831f58c9d275db9d
456477617264 <ec557782@gmail.com>
2022-09-27 20:59:23 +0100

    change repo to avoid calling same one twice

--- src/data/contribs.js
+++ src/data/contribs.js
@@ -1,4 +1,4 @@
-const SiteRepo = "CodheadClub/AwesomeResources"; // Define the repo to get contributors from
+const SiteRepo = "CodheadClub/ITSkillsTraining"; // Define the repo to get contributors from
 const Cache = require("@11ty/eleventy-cache-assets");
 
 module.exports = async function () {
